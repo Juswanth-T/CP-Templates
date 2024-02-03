b0550b2cb9977df403c74162ab6ff5698884cad8
Juswanth-T <95039624+Juswanth-T@users.noreply.github.com>
2024-02-03 22:58:38 +0530

    Create template.cpp

---
+++ Template/template.cpp
@@ -0,0 +1,30 @@
+// Juswanth T
+ 
+#ifdef Local
+#include "Resource\debug1.h"
+#else 
+#define dbg(...)
+#endif
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long int
+#define float long double
+#define decimal(x) fixed << setprecision(x)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+void solve(int t){
+      
+      
+}
+ 
+ 
+ int32_t main(){
+  fast;
+  int tc;
+  cin>>tc;
+  for(int t = 1 ; t <= tc; t++){
+    solve(t);
+  }
+   
+}
